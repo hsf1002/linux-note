d6661701e62778cbb1482504e65c4c650a9486bd
sky <hsf1002@gmail.com>
2019-12-15 21:56:29 +0800

    sigev_thread

---
+++ 23-定时器与休眠/ptmr_sigev_thread.c
@@ -0,0 +1,157 @@
+#define _POSIX_C_SOURCE 199309
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include "cur_time.h"
+
+
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+// 过期定时器的数量
+static int exprire_cnt = 0;
+
+
+/**
+ * 
+ * 将时间+间隔，转为itimerspec的值
+ */
+static void
+itimerspec_from_str(const char *str, struct itimerspec *tsp)
+{
+    char *cptr;
+    char *sptr;
+
+    if (NULL != (cptr = strchr(str, ':')))
+        *cptr = '\0';
+    
+    if (NULL != (sptr = strchr(str, '/')))
+        *cptr = '\0';
+    
+    tsp->it_value.tv_sec = atoi(str);
+    tsp->it_value.tv_nsec = (sptr != NULL) ? atoi(sptr + 1) : 0;
+
+    if (NULL == cptr)
+    {
+        tsp->it_interval.tv_sec = 0;
+        tsp->it_interval.tv_nsec = 0;
+    }
+    else
+    {
+        if (NULL != (sptr = strchr(cptr, '/')))
+            *sptr = '\0';
+
+        tsp->it_interval.sec = atoi(cptr + 1);
+        tsp->it_interval.nsec = (sptr != NULL) ? atoi(sptr + 1) : 0;
+    }
+}
+
+/**
+ * 4. 定时器到期调用此函数
+ */
+static void
+thread_fun(union sigval sv)
+{
+    timer_t *tidptr;
+    int s;
+
+    tidptr = sv.sival_ptr;
+
+    printf("[%s] Thread notify \n", curr_time("%T"));
+    printf("    timer ID = %ld\n", (long)*tidptr);
+    printf("    timer_getoverrun() = %d \n", timer_getoverrun(*tidptr));
+
+    if (0 != (s = pthread_mutex_lock(&mtx)))
+        perror("mutex lock error");
+    
+    exprire_cnt += 1 + timer_getoverrun(*tidptr);
+
+    if (0 != (s = pthread_mutex_unlock(&mtx)))
+        perror("mutex unlock error");
+    
+    // 条件满足，通知主线程
+    if (0 != (s = pthread_cond_signal(&cond)))
+        perror("cond signal error");
+}
+
+
+/**
+ *   
+ * 
+ * 使用线程函数进行POSIX定时器通知
+ ./ptmr_sigev_thread 5:5 10:10
+
+ Control+Z to suspend
+
+ fg
+
+Control+C to kill
+
+ */
+int
+main(int argc, char *argv[])    
+{
+    struct itimerspec ts;
+    struct sigevent sev;
+    timer_t *tidlist;
+
+    if (argc < 2)
+        fprintf(stderr, "%s sec[/nsec][:int-sec][/int-nsec]...\n", argv[0]);
+    
+    // 根据指定参数，给多个定时器分配空间
+    if (NULL == (tidlist = calloc(argc - 1, sizeof(timer_t))))
+        perror("calloc error");
+
+    // 通过线程函数发送通知
+    sev.sigev_notify = SIGEV_THREAD;
+    // 指定线程函数
+    sev.sigev_notify_function = thread_fun;
+    sev.sigev_notify_attributes = NULL;
+
+    // 1. 启动若干个定时器
+    for (int i=0; i<argc-1; i++)
+    {
+        // 命令行指定参数转换为itimerspec结构
+        itimerspec_from_str(argv[i+1], &ts);
+
+        // 指定伴随数据为定时器的timer_id
+        sev.sigev_value.sival_ptr = &tidlist[i];
+
+        // 创建定时器
+        if (-1 == timer_create(CLOCK_REALTIME, &sev, &tidlist[i]))
+            perror("timer_create error");
+        
+        printf("Timer ID: %ld (%s) \n", (long)tidlist[i], argv[i+1]);
+
+        // 启动定时器
+        if (-1 == timer_settime(tidlist[i], 0, &ts, NULL))
+            perror("timer_settime error");
+    }
+
+    // 2. 互斥量加锁
+    if (0 != (s = pthread_mutex_lock(&mtx)))
+        perror("mutex lock error");
+    
+    // 3. 阻塞
+    for (;;)
+    {
+        // 等待满足条件变量
+        if (0 != (s = pthread_cond_wait(&cond, &mtx)))
+            perror("cond wait error");
+        
+        printf("main: expire cnt = %d \n", exprire_cnt);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
