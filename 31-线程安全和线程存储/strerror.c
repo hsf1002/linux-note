dedf2d7eaaec5a79d453cf88aab3f0c93b088885
sky <hsf1002@gmail.com>
2020-01-08 21:32:30 +0800

    strerror

---
+++ 31-线程安全和线程存储/strerror.c
@@ -0,0 +1,75 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define MAX_ERROR_LEN 256
+
+static char buf[MAX_ERROR_LEN];
+
+
+/**
+ * strerr的非线程安全版本
+ */
+char *
+strerror(int err)
+{
+    if (err < 0 || err >= _sys_nerr || _sys_errlist[err] == NULL)
+    {
+        snprintf(buf, MAX_ERROR_LEN, "unknown error %d", err);
+    }
+    else
+    {
+        strncpy(buf, _sys_errlist[err], MAX_ERROR_LEN - 1);
+        buf[MAX_ERROR_LEN - 1] = '\0';
+    }
+
+    return buf;
+}
+
+/**
+ * 
+ */
+static void *
+thr_func(void *arg)
+{
+    char *str;
+
+    printf("other thread about to call strerror()\n");
+    str = strerror(EPERM);
+    printf("other therad str(%p): %s\n", str, str);
+
+    return NULL;
+}
+
+/**
+ *    
+ * 
+ */
+int
+main(int argc, char *argv[])    
+{
+    pthread_t t;
+    int s;
+    char *str;
+
+    str = strerror(EINVAL);
+    printf("main thread has called strerror()\n");
+
+    if (0 != (s = pthread_create(&t, NULL, thr_func, NULL)))
+        perror("pthread_create error");
+    
+    if (0 != (s = pthread_join(t, NULL)))
+        perror("pthread_join error");
+    
+    printf("main thread str(%p): %s\n", str, str);
+
+    exit(EXIT_SUCCESS);
+}
+
